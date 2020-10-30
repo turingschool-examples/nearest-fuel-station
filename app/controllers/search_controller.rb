fa2a450fd3b42f9ea0eb8d61b1fbc0c66401f1c2
Nico Rithner <nicorithner@gmail.com>
2020-10-30 11:04:38 -0600

    Add directions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,7 @@ class SearchController < ApplicationController
     # params = location => "address as string"
     station_data = StationsService.nearest_station(params[:location])
     @station = Station.new(station_data)
+    directions_data = DestinationService.get_directions(params[:location], @station.full_address)
+    @directions = Destination.new(directions_data)
   end
 end
