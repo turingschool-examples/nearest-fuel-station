b511e31327a5fc4e36531155632c2c08271fd94b
Nico Rithner <nicorithner@gmail.com>
2020-10-30 11:04:20 -0600

    Update poro to have full_address

--- app/poros/station.rb
+++ app/poros/station.rb
@@ -1,6 +1,6 @@
 class Station
 
-  attr_reader :name, :address, :city, :state, :zip, :fuel, :times
+  attr_reader :name, :address, :city, :state, :zip, :fuel, :times, :full_address
 
   def initialize(data)
     @name = data[:fuel_stations][0][:station_name]
@@ -10,5 +10,6 @@ class Station
     @zip = data[:fuel_stations][0][:zip]
     @fuel = data[:fuel_stations][0][:fuel_type_code]
     @times = data[:fuel_stations][0][:access_days_time]
+    @full_address = "#{@address}, #{@city}, #{@state}, #{@zip}"
   end
 end
