8f09b0ee82b62f72e79e8ff78f6e3cf21b2b47ea
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-17 22:54:03 -0400

    Add turing to drop down

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def location_list
     [
       ['Griffin Coffee', '5224 W 25th Ave, Denver, CO 80214'],
-      ['The Noshery', '4994 Lowell Blvd, Denver, CO 80221']
+      ['The Noshery', '4994 Lowell Blvd, Denver, CO 80221'],
+      ['Turing', '1331 17th St, Denver, CO 80202']
     ]
   end
 end
