e8bf3c6afeb7fdbac02fad8704c7e43050c5dbf6
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-17 23:41:00 -0400

    preliminary test for mapquest service written

---
+++ spec/services/map_quest_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Mapquest Service" do
+  describe 'class methods' do
+    it "returns directions between starting addressand end address" do
+      start_address = '1331 17th St, Denver, CO 80202'
+      end_address   = '1225 17th Street, Denver, CO 80202'
+
+      data = MapquestService.route(start_address, end_address)
+
+      distance    = data[:route][:distance]
+      # travel_time = data
+
+    end
+  end
+
+end
