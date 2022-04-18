ca236e642a9a56c20b4756b1033326499bc7470f
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-18 00:20:14 -0400

    Mapquest facade spec

---
+++ spec/facades/mapquest_facade_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Mapquest Facade' do
+  describe 'class methods' do
+    it "creates route objects" do
+      start_address = '1331 17th St, Denver, CO 80202'
+      end_address = '1225 17th Street, Denver, CO 80202'
+      route = MapquestFacade.route(start_address, end_address)
+
+      expect(route).to be_a Route
+    end
+  end
+end
