f76b52731f712a44b534aebfab94af8d8eb99e9d
Sully Birashk <sully.birashk@gmail.com>
2022-04-17 18:01:34 -0700

    Find Nearest Station

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
     @location = params[:location]
+    @station_info = StationFacade.nearest_station(@location)
   end
 
 end
---
+++ app/services/station_service.rb
@@ -0,0 +1,16 @@
+class StationService
+
+  def self.conn
+    Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json")
+  end
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.find_nearest_station(address)
+    get_url("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['api_key']}&location=#{address}&limit=1")
+  end
+
+end
