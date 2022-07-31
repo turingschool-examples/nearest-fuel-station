b7489c1fe06669bcabba18b8c11edfa5ba21e9d8
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-31 09:21:15 -0600

    Add mapquest service and spec

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,3 +1,19 @@
-class MapQuestService
+class MapQuestService 
+  def self.conn
+    url = "http://www.mapquestapi.com/"
+    Faraday.new(url: url)
+  end
 
+  def self.get_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_directions(from, to)
+    response = conn.get('/directions/v2/route?') do |f|
+      f.params[:key] = ENV['map_quest_api_key']
+      f.params[:from] = from
+      f.params[:to] = to
+    end
+    MapQuestService.get_json(response)
+  end
 end
--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe MapQuestService do 
-  xit 'returns the data for the station closest to Turing' do 
-    
+  it 'returns the data for the station closest to Turing' do 
+    from = '1331 17th St LL100, Denver, CO 80202'
+    to = '1550 Market St'
+
+    direction_data = MapQuestService.get_directions(from, to)
+    # binding.pry
+    expect(direction_data).to be_a(Hash)
+    expect(direction_data[:route]).to have_key(:distance)
+    expect(direction_data[:route]).to have_key(:formattedTime)
+    expect(direction_data[:route][:legs].first).to have_key(:maneuvers)
+
   end
 end
